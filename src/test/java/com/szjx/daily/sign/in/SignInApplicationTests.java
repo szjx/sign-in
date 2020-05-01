128b453da455b4452de10691f600d6fa84cfbca8
lihui23 <lihui23@vipkid.com.cn>
2020-05-01 16:33:10 +0800

    init sign-in

---
+++ src/test/java/com/szjx/daily/sign/in/SignInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szjx.daily.sign.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
